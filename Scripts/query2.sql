663af58f07ef42ee7657413644d61bfb5cb7b00f
Ananth C <102150197+ananthc1@users.noreply.github.com>
2023-08-03 16:04:59 +0000

    change new

--- Scripts/query2.sql
+++ Scripts/query2.sql
@@ -1,2 +1,14 @@
 -- How many cars has been sold per employee
-SELECT 
+-- start with this query
+SELECT emp.employeeId, emp.firstName, emp.lastName
+FROM sales sls
+INNER JOIN employee emp
+    ON sls.employeeId = emp.employeeId
+
+-- then add the group by & count
+SELECT emp.employeeId, emp.firstName, emp.lastName, count(*) AS NumOfCarsSold
+FROM sales sls 
+INNER JOIN employee emp
+ON sls.employeeId = emp.employeeId
+GROUP BY emp.employeeId, emp.firstName, emp.lastName
+ORDER BY NumOfCarsSold DESC;
